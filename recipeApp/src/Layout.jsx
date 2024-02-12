3f86e2c5d9f49997748580676a1dcf91405c30d0
kuchbhimingwal <shubhammingi@gmail.com>
2024-02-12 08:30:37 -0500

    wraped the whole app in the context provider so all of the componenets have excess to context

--- recipeApp/src/Layout.jsx
+++ recipeApp/src/Layout.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import FavContextProvider from "./context/favContextProvider";
 
 function Layout(){
   
   return (
-    <>
+    <FavContextProvider>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </FavContextProvider>
   )
 }
 
---
+++ recipeApp/src/components/Fav/Fav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Fav(){
+  return(
+    <h1>favlist</h1>
+  )
+}
+
+export default Fav
