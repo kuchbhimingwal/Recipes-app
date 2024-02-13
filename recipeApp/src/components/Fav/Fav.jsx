1b5d984be81fe62b1645c8fbccf039a5bda065f1
kuchbhimingwal <shubhammingi@gmail.com>
2024-02-13 07:36:06 -0500

    added the remove functionality in the fav page

--- recipeApp/src/components/Fav/Fav.jsx
+++ recipeApp/src/components/Fav/Fav.jsx
@@ -1,14 +1,12 @@
-import React, { useContext } from "react";
+import React, { useCallback, useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import FavContext from "../../context/favContext";
 
 function Fav(){
-
-  const {favList} = useContext(FavContext)
+  const {favList, removeList} = useContext(FavContext)
 
   return(
     <div className="flex flex-wrap justify-center gap-9 m-4">
-      <h1>Favourites</h1>
       {favList.map((item,index)=>(
         <div className="w-[300px] rounded-md border flex flex-wrap">
         <img
@@ -25,18 +23,18 @@ function Fav(){
             <button
               type="button"
               className="w-full h-8 mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              onClick={() => addList(index)}
+              onClick={() => removeList(index)}
             >
-              Add to fav
+              Remove from fav
             </button>
-          <Link to={`/recipepage/${index}`}>
+          {/* <Link to={`javascript:history.back()/recipepage/${index}`}>
               <button
                 type="button"
                 className="w-full h-8 mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
               >
                 Recipe
               </button>
-            </Link>
+            </Link> */}
         </div>
       </div>
       ))}
--- recipeApp/src/context/favContextProvider.jsx
+++ recipeApp/src/context/favContextProvider.jsx
@@ -4,8 +4,12 @@ import FavContext from "./favContext";
 const FavContextProvider = ({children}) =>{
   const[favList, setFavList] = useState([])
 
+  const removeList = (index)=>{
+    setFavList([...favList.slice(0, index), ...favList.slice(index + 1)])
+    console.log(favList)
+  }
   return(
-    <FavContext.Provider value={{favList,setFavList}}>
+    <FavContext.Provider value={{favList,setFavList,removeList}}>
       {children}
     </FavContext.Provider>
   )
